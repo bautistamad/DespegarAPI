c0b86742cf299b7fda723037b5bc618321155ed4
Bautista Madruga <bautistamadruga20@gmail.com>
2022-01-25 15:25:21 -0300

    User Login ready

--- login/views.py
+++ login/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import *
 from .models import User
-import jwt, datetime
+import jwt
+import datetime
+
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -19,10 +22,11 @@ class LoginView(APIView):
         password = request.data['password']
 
         user = User.objects.filter(email=email).first()
+
         if user is None:
             raise AuthenticationFailed('User not found!')
 
-        if user.password != password:
+        if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
 
         payload = {
@@ -34,10 +38,40 @@ class LoginView(APIView):
         token = jwt.encode(payload, 'secret', algorithm='HS256')
 
         response = Response()
-        
-        response.set_cookie(key="jwt", httponly=True)
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
         response.data = {
             'jwt': token
         }
 
+        return response
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
+
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
         return response
